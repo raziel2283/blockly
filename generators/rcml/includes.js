aa0576fca0cf065d452f59692ad3ed2e7ebcf1e8
Egor Semeiko <raziel2283@mail.ru>
2017-03-29 19:22:31 +0300

    Removed unnecessary padding in includes.

--- generators/rcml/includes.js
+++ generators/rcml/includes.js
@@ -8,7 +8,7 @@ goog.require('Blockly.RCML');
 
 Blockly.RCML['include'] = function(block) {
     var path = Blockly.RCML.valueToCode(block, 'TEXT', Blockly.RCML.ORDER_NONE);
-    var code = 'include ' +path+ '\n';
+    var code = 'include ' +path;
     Blockly.RCML.definitions_['include'+block.id] = code;
     return null;
 };
@@ -16,7 +16,7 @@ Blockly.RCML['include'] = function(block) {
 Blockly.RCML['include_lib'] = function(block) {
     var name = block.getFieldValue('FIELDNAME', 'text');
     var path = Blockly.RCML.valueToCode(block, 'TEXT', Blockly.RCML.ORDER_NONE);
-    var code = 'include_lib ' +name+ ' ' +path+ '\n';
+    var code = 'include_lib ' +name+ ' ' +path;
     if(/^[a-zA-Z][a-zA-Z0-9_]+$/.test(name)) Blockly.RCML.definitions_[name] = code;
     return null;
 };
